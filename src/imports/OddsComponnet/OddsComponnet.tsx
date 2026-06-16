2e7a12b56afb84c727aa75b68c6f81ffbe66666f
adamniepsuj-max <adam.niepsuj@888williamhill.com>
2026-06-16 14:28:00 +0100

    Deploy via CodexShare: src/imports/OddsComponnet/OddsComponnet.tsx

---
+++ src/imports/OddsComponnet/OddsComponnet.tsx
@@ -0,0 +1,226 @@
+function Paragraph() {
+  return (
+    <div className="h-[13.5px] relative shrink-0 w-full" data-name="Paragraph">
+      <p className="-translate-x-1/2 absolute font-['SF_Pro:Regular',sans-serif] font-normal leading-[12px] left-[6.5px] text-[#546d91] text-[10px] text-center top-0 whitespace-nowrap" style={{ fontVariationSettings: "'wdth' 100" }}>
+        Match 1
+      </p>
+    </div>
+  );
+}
+
+function Container() {
+  return (
+    <div className="content-stretch flex flex-col h-[13.5px] items-start opacity-80 pl-[19.742px] pr-[19.383px] relative shrink-0 w-[51.797px]" data-name="Container4">
+      <Paragraph />
+    </div>
+  );
+}
+
+function Container1() {
+  return (
+    <div className="h-[15px] relative shrink-0 w-[15.594px]" data-name="Container5">
+      <p className="-translate-x-1/2 absolute font-['SF_Pro:Semi_Bold',sans-serif] leading-[12px] left-[8.5px] not-italic text-[#183060] text-[10px] text-center top-[3px] whitespace-nowrap">6/1</p>
+    </div>
+  );
+}
+
+function Frame() {
+  return (
+    <div className="content-stretch flex flex-col gap-[2px] items-center justify-center relative shrink-0 w-[50px]">
+      <Container />
+      <Container1 />
+    </div>
+  );
+}
+
+function Component() {
+  return (
+    <div className="absolute content-stretch flex flex-col h-[40px] items-center justify-center left-0 px-[16px] py-[12px] rounded-[4px] top-0 w-[64px]" data-name="5/1">
+      <Frame />
+    </div>
+  );
+}
+
+function Paragraph1() {
+  return (
+    <div className="h-[13.5px] relative shrink-0 w-full" data-name="Paragraph">
+      <p className="-translate-x-1/2 absolute font-['SF_Pro:Regular',sans-serif] font-normal leading-[12px] left-[6.5px] text-[#546d91] text-[10px] text-center top-0 whitespace-nowrap" style={{ fontVariationSettings: "'wdth' 100" }}>
+        Match 2
+      </p>
+    </div>
+  );
+}
+
+function Container2() {
+  return (
+    <div className="content-stretch flex flex-col h-[13.5px] items-start opacity-80 pl-[19.742px] pr-[19.383px] relative shrink-0 w-[51.797px]" data-name="Container4">
+      <Paragraph1 />
+    </div>
+  );
+}
+
+function Container3() {
+  return (
+    <div className="h-[15px] relative shrink-0 w-[15.594px]" data-name="Container5">
+      <p className="-translate-x-1/2 absolute font-['SF_Pro:Semi_Bold',sans-serif] leading-[12px] left-[8px] not-italic text-[#183060] text-[10px] text-center top-[3px] whitespace-nowrap">60/1</p>
+    </div>
+  );
+}
+
+function Frame1() {
+  return (
+    <div className="content-stretch flex flex-col gap-[2px] items-center justify-center relative shrink-0 w-[50px]">
+      <Container2 />
+      <Container3 />
+    </div>
+  );
+}
+
+function Component1() {
+  return (
+    <div className="absolute content-stretch flex flex-col h-[40px] items-center justify-center left-[70px] px-[16px] py-[12px] rounded-[4px] top-0 w-[64px]" data-name="40/1">
+      <Frame1 />
+    </div>
+  );
+}
+
+function Paragraph2() {
+  return (
+    <div className="h-[13.5px] relative shrink-0 w-full" data-name="Paragraph">
+      <p className="-translate-x-1/2 absolute font-['SF_Pro:Regular',sans-serif] font-normal leading-[12px] left-[6.5px] text-[#546d91] text-[10px] text-center top-0 whitespace-nowrap" style={{ fontVariationSettings: "'wdth' 100" }}>
+        Match 3
+      </p>
+    </div>
+  );
+}
+
+function Container4() {
+  return (
+    <div className="content-stretch flex flex-col h-[13.5px] items-start opacity-80 pl-[19.742px] pr-[19.383px] relative shrink-0 w-[51.797px]" data-name="Container4">
+      <Paragraph2 />
+    </div>
+  );
+}
+
+function Container5() {
+  return (
+    <div className="h-[15px] relative shrink-0 w-[15.594px]" data-name="Container5">
+      <p className="-translate-x-1/2 absolute font-['SF_Pro:Semi_Bold',sans-serif] leading-[12px] left-[8px] not-italic text-[#183060] text-[10px] text-center top-[3px] whitespace-nowrap">700/1</p>
+    </div>
+  );
+}
+
+function Frame2() {
+  return (
+    <div className="content-stretch flex flex-col gap-[2px] items-center justify-center relative shrink-0 w-[50px]">
+      <Container4 />
+      <Container5 />
+    </div>
+  );
+}
+
+function Component2() {
+  return (
+    <div className="absolute content-stretch flex flex-col h-[40px] items-center justify-center left-[140px] px-[16px] py-[12px] rounded-[4px] top-0 w-[64px]" data-name="350/1">
+      <Frame2 />
+    </div>
+  );
+}
+
+function Paragraph3() {
+  return (
+    <div className="h-[13.5px] relative shrink-0 w-full" data-name="Paragraph">
+      <p className="-translate-x-1/2 absolute font-['SF_Pro:Regular',sans-serif] font-normal leading-[12px] left-[6.5px] text-[#546d91] text-[10px] text-center top-0 whitespace-nowrap" style={{ fontVariationSettings: "'wdth' 100" }}>
+        Match 4
+      </p>
+    </div>
+  );
+}
+
+function Container6() {
+  return (
+    <div className="content-stretch flex flex-col h-[13.5px] items-start opacity-80 pl-[19.742px] pr-[19.383px] relative shrink-0 w-[51.797px]" data-name="Container4">
+      <Paragraph3 />
+    </div>
+  );
+}
+
+function Container7() {
+  return (
+    <div className="h-[15px] relative shrink-0 w-[15.594px]" data-name="Container5">
+      <p className="-translate-x-1/2 absolute font-['SF_Pro:Semi_Bold',sans-serif] leading-[12px] left-[8px] not-italic text-[#183060] text-[10px] text-center top-[3px] whitespace-nowrap">7,500/1</p>
+    </div>
+  );
+}
+
+function Frame3() {
+  return (
+    <div className="content-stretch flex flex-col gap-[2px] items-center justify-center relative shrink-0 w-[50px]">
+      <Container6 />
+      <Container7 />
+    </div>
+  );
+}
+
+function Component3() {
+  return (
+    <div className="absolute content-stretch flex flex-col h-[40px] items-center justify-center left-[210px] px-[16px] py-[12px] rounded-[4px] top-0 w-[64px]" data-name="5,000/1">
+      <Frame3 />
+    </div>
+  );
+}
+
+function Paragraph4() {
+  return (
+    <div className="h-[13.5px] relative shrink-0 w-full" data-name="Paragraph">
+      <p className="-translate-x-1/2 absolute font-['SF_Pro:Regular',sans-serif] font-normal leading-[12px] left-[6.5px] text-[#546d91] text-[10px] text-center top-0 whitespace-nowrap" style={{ fontVariationSettings: "'wdth' 100" }}>
+        Match 5
+      </p>
+    </div>
+  );
+}
+
+function Container8() {
+  return (
+    <div className="content-stretch flex flex-col h-[13.5px] items-start opacity-80 pl-[19.742px] pr-[19.383px] relative shrink-0 w-[51.797px]" data-name="Container4">
+      <Paragraph4 />
+    </div>
+  );
+}
+
+function Container9() {
+  return (
+    <div className="h-[15px] relative shrink-0 w-[15.594px]" data-name="Container5">
+      <p className="-translate-x-1/2 absolute font-['SF_Pro:Semi_Bold',sans-serif] leading-[12px] left-[8.5px] not-italic text-[#183060] text-[10px] text-center top-[3px] whitespace-nowrap">125,000/1</p>
+    </div>
+  );
+}
+
+function Frame4() {
+  return (
+    <div className="content-stretch flex flex-col gap-[2px] items-center justify-center relative shrink-0 w-[50px]">
+      <Container8 />
+      <Container9 />
+    </div>
+  );
+}
+
+function Component4() {
+  return (
+    <div className="absolute content-stretch flex flex-col h-[40px] items-center justify-center left-[280px] px-[16px] py-[12px] rounded-[4px] top-0 w-[64px]" data-name="50,000/1">
+      <Frame4 />
+    </div>
+  );
+}
+
+export default function OddsComponnet() {
+  return (
+    <div className="relative size-full" data-name="Odds componnet">
+      <Component />
+      <Component1 />
+      <Component2 />
+      <Component3 />
+      <Component4 />
+    </div>
+  );
+}
