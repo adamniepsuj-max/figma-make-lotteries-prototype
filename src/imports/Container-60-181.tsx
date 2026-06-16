3abd2ff300eb5ab93b8a8fddf7256a0973303aaa
adamniepsuj-max <adam.niepsuj@888williamhill.com>
2026-06-16 14:26:20 +0100

    Deploy via CodexShare: src/imports/Container-60-181.tsx

---
+++ src/imports/Container-60-181.tsx
@@ -0,0 +1,403 @@
+import svgPaths from "./svg-pyrtoqlwov";
+
+function Heading() {
+  return (
+    <div className="h-[28px] relative shrink-0 w-[115.531px]" data-name="Heading 1">
+      <div className="bg-clip-padding border-0 border-[transparent] border-solid relative size-full">
+        <p className="absolute font-['Inter:Bold',sans-serif] font-bold leading-[28px] left-0 not-italic text-[20px] text-white top-0 tracking-[-0.4492px] whitespace-nowrap">Irish Lotto</p>
+      </div>
+    </div>
+  );
+}
+
+function Icon() {
+  return (
+    <div className="h-[16px] overflow-clip relative shrink-0 w-full" data-name="Icon">
+      <div className="absolute inset-[8.33%]" data-name="Vector">
+        <div className="absolute inset-[-5%]">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 14.6667 14.6667">
+            <path d={svgPaths.p3d62dd80} id="Vector" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.33333" />
+          </svg>
+        </div>
+      </div>
+      <div className="absolute bottom-[33.33%] left-1/2 right-1/2 top-1/2" data-name="Vector">
+        <div className="absolute inset-[-25%_-0.67px]">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 1.33333 4">
+            <path d="M0.666667 3.33333V0.666667" id="Vector" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.33333" />
+          </svg>
+        </div>
+      </div>
+      <div className="absolute bottom-[66.67%] left-1/2 right-[49.96%] top-[33.33%]" data-name="Vector">
+        <div className="absolute inset-[-0.67px]">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 1.34 1.33333">
+            <path d="M0.666667 0.666667H0.673334" id="Vector" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.33333" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Button() {
+  return (
+    <div className="relative rounded-[10px] shrink-0 size-[28px]" data-name="Button">
+      <div className="bg-clip-padding border-0 border-[transparent] border-solid content-stretch flex flex-col items-start pt-[6px] px-[6px] relative size-full">
+        <Icon />
+      </div>
+    </div>
+  );
+}
+
+function Container1() {
+  return (
+    <div className="absolute content-stretch flex h-[28px] items-center justify-between left-[16px] top-[16px] w-[343px]" data-name="Container">
+      <Heading />
+      <Button />
+    </div>
+  );
+}
+
+function Container4() {
+  return (
+    <div className="h-[13.5px] opacity-80 relative shrink-0 w-full" data-name="Container">
+      <p className="-translate-x-1/2 absolute font-['Inter:Medium',sans-serif] font-medium leading-[13.5px] left-[26.08px] not-italic text-[9px] text-center text-white top-0 tracking-[0.167px] whitespace-nowrap">M1</p>
+    </div>
+  );
+}
+
+function Container5() {
+  return (
+    <div className="h-[15px] relative shrink-0 w-full" data-name="Container">
+      <p className="-translate-x-1/2 absolute font-['Inter:Bold',sans-serif] font-bold leading-[15px] left-[26.14px] not-italic text-[10px] text-center text-white top-0 tracking-[0.1172px] whitespace-nowrap">5/1</p>
+    </div>
+  );
+}
+
+function Container3() {
+  return (
+    <div className="absolute bg-[rgba(255,255,255,0.1)] content-stretch flex flex-col h-[40.5px] items-start left-0 pt-[6px] px-[6px] rounded-[10px] top-0 w-[63.797px]" data-name="Container">
+      <Container4 />
+      <Container5 />
+    </div>
+  );
+}
+
+function Container7() {
+  return (
+    <div className="h-[13.5px] opacity-80 relative shrink-0 w-full" data-name="Container">
+      <p className="-translate-x-1/2 absolute font-['Inter:Medium',sans-serif] font-medium leading-[13.5px] left-[25.97px] not-italic text-[9px] text-center text-white top-0 tracking-[0.167px] whitespace-nowrap">M2</p>
+    </div>
+  );
+}
+
+function Container8() {
+  return (
+    <div className="h-[15px] relative shrink-0 w-full" data-name="Container">
+      <p className="-translate-x-1/2 absolute font-['Inter:Bold',sans-serif] font-bold leading-[15px] left-[26.05px] not-italic text-[10px] text-center text-white top-0 tracking-[0.1172px] whitespace-nowrap">40/1</p>
+    </div>
+  );
+}
+
+function Container6() {
+  return (
+    <div className="absolute bg-[rgba(255,255,255,0.1)] content-stretch flex flex-col h-[40.5px] items-start left-[69.8px] pt-[6px] px-[6px] rounded-[10px] top-0 w-[63.797px]" data-name="Container">
+      <Container7 />
+      <Container8 />
+    </div>
+  );
+}
+
+function Container10() {
+  return (
+    <div className="h-[13.5px] opacity-80 relative shrink-0 w-full" data-name="Container">
+      <p className="-translate-x-1/2 absolute font-['Inter:Medium',sans-serif] font-medium leading-[13.5px] left-[26.36px] not-italic text-[9px] text-center text-white top-0 tracking-[0.167px] whitespace-nowrap">M3</p>
+    </div>
+  );
+}
+
+function Container11() {
+  return (
+    <div className="h-[15px] relative shrink-0 w-full" data-name="Container">
+      <p className="-translate-x-1/2 absolute font-['Inter:Bold',sans-serif] font-bold leading-[15px] left-[26.25px] not-italic text-[10px] text-center text-white top-0 tracking-[0.1172px] whitespace-nowrap">350/1</p>
+    </div>
+  );
+}
+
+function Container9() {
+  return (
+    <div className="absolute bg-[rgba(255,255,255,0.1)] content-stretch flex flex-col h-[40.5px] items-start left-[139.59px] pt-[6px] px-[6px] rounded-[10px] top-0 w-[63.797px]" data-name="Container">
+      <Container10 />
+      <Container11 />
+    </div>
+  );
+}
+
+function Container13() {
+  return (
+    <div className="h-[13.5px] opacity-80 relative shrink-0 w-full" data-name="Container">
+      <p className="-translate-x-1/2 absolute font-['Inter:Medium',sans-serif] font-medium leading-[13.5px] left-[26.28px] not-italic text-[9px] text-center text-white top-0 tracking-[0.167px] whitespace-nowrap">M4</p>
+    </div>
+  );
+}
+
+function Container14() {
+  return (
+    <div className="h-[15px] relative shrink-0 w-full" data-name="Container">
+      <p className="-translate-x-1/2 absolute font-['Inter:Bold',sans-serif] font-bold leading-[15px] left-[26.33px] not-italic text-[10px] text-center text-white top-0 tracking-[0.1172px] whitespace-nowrap">5,000/1</p>
+    </div>
+  );
+}
+
+function Container12() {
+  return (
+    <div className="absolute bg-[rgba(255,255,255,0.1)] content-stretch flex flex-col h-[40.5px] items-start left-[209.39px] pt-[6px] px-[6px] rounded-[10px] top-0 w-[63.797px]" data-name="Container">
+      <Container13 />
+      <Container14 />
+    </div>
+  );
+}
+
+function Container16() {
+  return (
+    <div className="h-[13.5px] opacity-80 relative shrink-0 w-full" data-name="Container">
+      <p className="-translate-x-1/2 absolute font-['Inter:Medium',sans-serif] font-medium leading-[13.5px] left-[25.89px] not-italic text-[9px] text-center text-white top-0 tracking-[0.167px] whitespace-nowrap">M5</p>
+    </div>
+  );
+}
+
+function Container17() {
+  return (
+    <div className="h-[15px] relative shrink-0 w-full" data-name="Container">
+      <p className="-translate-x-1/2 absolute font-['Inter:Bold',sans-serif] font-bold leading-[15px] left-[26.41px] not-italic text-[10px] text-center text-white top-0 tracking-[0.1172px] whitespace-nowrap">50,000/1</p>
+    </div>
+  );
+}
+
+function Container15() {
+  return (
+    <div className="absolute bg-[rgba(255,255,255,0.1)] content-stretch flex flex-col h-[40.5px] items-start left-[279.19px] pt-[6px] px-[6px] rounded-[10px] top-0 w-[63.797px]" data-name="Container">
+      <Container16 />
+      <Container17 />
+    </div>
+  );
+}
+
+function Container2() {
+  return (
+    <div className="absolute h-[40.5px] left-[16px] top-[142px] w-[343px]" data-name="Container">
+      <Container3 />
+      <Container6 />
+      <Container9 />
+      <Container12 />
+      <Container15 />
+    </div>
+  );
+}
+
+function Icon1() {
+  return (
+    <div className="absolute left-[57.63px] size-[12px] top-[7.5px]" data-name="Icon">
+      <svg className="absolute block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 12 12">
+        <g id="Icon">
+          <path d={svgPaths.p3892d900} id="Vector" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" />
+          <path d="M1.5 1.5V4H4" id="Vector_2" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" />
+          <path d="M6 3.5V6L8 7" id="Vector_3" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Button1() {
+  return (
+    <div className="bg-[rgba(255,255,255,0.1)] flex-[168.5_0_0] h-[27px] min-h-px min-w-px relative rounded-[10px]" data-name="Button">
+      <div className="bg-clip-padding border-0 border-[transparent] border-solid relative size-full">
+        <Icon1 />
+        <p className="-translate-x-1/2 absolute font-['Inter:Semi_Bold',sans-serif] font-semibold leading-[15px] left-[93.13px] not-italic text-[10px] text-center text-white top-[6px] tracking-[0.1172px] whitespace-nowrap">Results</p>
+      </div>
+    </div>
+  );
+}
+
+function Icon2() {
+  return (
+    <div className="absolute left-[63.06px] size-[12px] top-[7.5px]" data-name="Icon">
+      <svg className="absolute block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 12 12">
+        <g id="Icon">
+          <path d={svgPaths.p38532b20} id="Vector" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" />
+          <path d="M9 8.5V4.5" id="Vector_2" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" />
+          <path d="M6.5 8.5V2.5" id="Vector_3" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" />
+          <path d="M4 8.5V7" id="Vector_4" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Button2() {
+  return (
+    <div className="bg-[rgba(255,255,255,0.1)] flex-[168.5_0_0] h-[27px] min-h-px min-w-px relative rounded-[10px]" data-name="Button">
+      <div className="bg-clip-padding border-0 border-[transparent] border-solid relative size-full">
+        <Icon2 />
+        <p className="-translate-x-1/2 absolute font-['Inter:Semi_Bold',sans-serif] font-semibold leading-[15px] left-[92.06px] not-italic text-[10px] text-center text-white top-[6px] tracking-[0.1172px] whitespace-nowrap">Stats</p>
+      </div>
+    </div>
+  );
+}
+
+function Container18() {
+  return (
+    <div className="absolute content-stretch flex gap-[6px] h-[27px] items-start left-[16px] top-[190.5px] w-[343px]" data-name="Container">
+      <Button1 />
+      <Button2 />
+    </div>
+  );
+}
+
+function Icon3() {
+  return (
+    <div className="relative shrink-0 size-[14px]" data-name="Icon">
+      <svg className="absolute block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 14 14">
+        <g clipPath="url(#clip0_36_1687)" id="Icon">
+          <path d={svgPaths.pc012c00} id="Vector" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.16667" />
+          <path d="M7 3.5V7L9.33333 8.16667" id="Vector_2" stroke="var(--stroke-0, white)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.16667" />
+        </g>
+        <defs>
+          <clipPath id="clip0_36_1687">
+            <rect fill="white" height="14" width="14" />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+  );
+}
+
+function Text() {
+  return (
+    <div className="flex-[1_0_0] h-[16px] min-h-px min-w-px relative" data-name="Text">
+      <div className="bg-clip-padding border-0 border-[transparent] border-solid content-stretch flex items-start relative size-full">
+        <p className="font-['Inter:Medium',sans-serif] font-medium leading-[16px] not-italic relative shrink-0 text-[12px] text-white whitespace-nowrap">Next draw</p>
+      </div>
+    </div>
+  );
+}
+
+function Container21() {
+  return (
+    <div className="h-[16px] relative shrink-0 w-[78.469px]" data-name="Container">
+      <div className="bg-clip-padding border-0 border-[transparent] border-solid content-stretch flex gap-[6px] items-center relative size-full">
+        <Icon3 />
+        <Text />
+      </div>
+    </div>
+  );
+}
+
+function Text1() {
+  return (
+    <div className="h-[16px] relative shrink-0 w-[115.344px]" data-name="Text">
+      <div className="bg-clip-padding border-0 border-[transparent] border-solid content-stretch flex items-start relative size-full">
+        <p className="font-['Inter:Bold',sans-serif] font-bold leading-[16px] not-italic relative shrink-0 text-[12px] text-white whitespace-nowrap">18:00 28 Mar 2026</p>
+      </div>
+    </div>
+  );
+}
+
+function Container20() {
+  return (
+    <div className="content-stretch flex h-[16px] items-center justify-between relative shrink-0 w-full" data-name="Container">
+      <Container21 />
+      <Text1 />
+    </div>
+  );
+}
+
+function Container22() {
+  return (
+    <div className="bg-[rgba(251,44,54,0.8)] h-[32px] relative rounded-[4px] shrink-0 w-full" data-name="Container">
+      <p className="-translate-x-1/2 absolute font-['Inter:Semi_Bold',sans-serif] font-semibold leading-[15px] left-[160.19px] not-italic text-[10px] text-center text-white top-[9px] tracking-[0.1172px] whitespace-nowrap">Starts in 1d 5h 45m 32s</p>
+    </div>
+  );
+}
+
+function Container19() {
+  return (
+    <div className="absolute bg-[rgba(255,255,255,0.1)] content-stretch flex flex-col gap-[6px] h-[78px] items-start left-[16px] pt-[12px] px-[12px] rounded-[10px] top-[56px] w-[343px]" data-name="Container">
+      <Container20 />
+      <Container22 />
+    </div>
+  );
+}
+
+function Icon4() {
+  return (
+    <div className="relative shrink-0 size-[16px]" data-name="Icon">
+      <svg className="absolute block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 16 16">
+        <g id="Icon">
+          <path d={svgPaths.pea6a680} id="Vector" stroke="var(--stroke-0, #FFDF20)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.33333" />
+          <path d={svgPaths.p3155f180} id="Vector_2" stroke="var(--stroke-0, #FFDF20)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.33333" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Text2() {
+  return (
+    <div className="flex-[1_0_0] h-[16px] min-h-px min-w-px relative" data-name="Text">
+      <div className="bg-clip-padding border-0 border-[transparent] border-solid content-stretch flex items-start relative size-full">
+        <p className="font-['Inter:Medium',sans-serif] font-medium leading-[16px] not-italic relative shrink-0 text-[12px] text-white whitespace-nowrap">Jackpot</p>
+      </div>
+    </div>
+  );
+}
+
+function Container25() {
+  return (
+    <div className="h-[16px] relative shrink-0 w-[68.266px]" data-name="Container">
+      <div className="bg-clip-padding border-0 border-[transparent] border-solid content-stretch flex gap-[6px] items-center relative size-full">
+        <Icon4 />
+        <Text2 />
+      </div>
+    </div>
+  );
+}
+
+function Text3() {
+  return (
+    <div className="h-[28px] relative shrink-0 w-[82.766px]" data-name="Text">
+      <div className="bg-clip-padding border-0 border-[transparent] border-solid relative size-full">
+        <p className="absolute font-['Inter:Bold',sans-serif] font-bold leading-[28px] left-0 not-italic text-[#ffdf20] text-[18px] top-0 tracking-[-0.4395px] whitespace-nowrap">£125,000</p>
+      </div>
+    </div>
+  );
+}
+
+function Container24() {
+  return (
+    <div className="content-stretch flex h-[28px] items-center justify-between relative shrink-0 w-full" data-name="Container">
+      <Container25 />
+      <Text3 />
+    </div>
+  );
+}
+
+function Container23() {
+  return (
+    <div className="absolute bg-[rgba(253,199,0,0.2)] content-stretch flex flex-col h-[50px] items-start left-[16px] pb-px pt-[11px] px-[11px] rounded-[10px] top-[142px] w-[343px]" data-name="Container">
+      <div aria-hidden="true" className="absolute border border-[rgba(253,199,0,0.3)] border-solid inset-0 pointer-events-none rounded-[10px]" />
+      <Container24 />
+    </div>
+  );
+}
+
+export default function Container() {
+  return (
+    <div className="bg-gradient-to-r from-[#155dfc] relative shadow-[0px_10px_15px_0px_rgba(0,0,0,0.1),0px_4px_6px_0px_rgba(0,0,0,0.1)] size-full to-[#9810fa]" data-name="Container">
+      <Container1 />
+      <Container2 />
+      <Container18 />
+      <Container19 />
+    </div>
+  );
+}
