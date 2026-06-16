d36ea5251a0b2c1e03c8f8cdbcc9143584395958
adamniepsuj-max <adam.niepsuj@888williamhill.com>
2026-06-16 14:28:27 +0100

    Deploy via CodexShare: src/imports/Lotto-1/Lotto-230-1467.tsx

---
+++ src/imports/Lotto-1/Lotto-230-1467.tsx
@@ -0,0 +1,98 @@
+import svgPaths from "./svg-7u79nypfi4";
+
+export default function Lotto() {
+  return (
+    <div className="bg-white content-stretch flex gap-[12px] items-center pl-[12px] pr-[8px] py-[8px] relative rounded-[8px] shadow-[0px_0px_18px_0px_rgba(23,31,51,0.1)] h-[70px] w-full" data-name="Lotto">
+      <div className="relative shrink-0">
+        <div className="content-stretch flex items-center relative">
+          <div className="relative shrink-0 size-[14px]" data-name="favs">
+            <div className="absolute inset-[2.5%_0]" data-name="Vector">
+              <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 14 13.3">
+                <path d={svgPaths.p326ec300} fill="var(--fill-0, #F9BF5B)" id="Vector" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="content-stretch flex items-center relative shrink-0">
+        <div className="relative rounded-[50px] shrink-0 size-[36px]" data-name="united-states">
+          <div aria-hidden="true" className="absolute border-[#ededed] border-[0.5px] border-solid inset-0 pointer-events-none rounded-[50px]" />
+          <div className="absolute inset-[-16.67%]">
+            <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 47.9999 48">
+              <g filter="url(#filter0_d_230_1510)" id="Vector">
+                <path d={svgPaths.p3ef20a80} fill="var(--fill-0, white)" />
+              </g>
+              <defs>
+                <filter colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse" height="48" id="filter0_d_230_1510" width="47.9999" x="0" y="0">
+                  <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                  <feColorMatrix in="SourceAlpha" result="hardAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" />
+                  <feOffset />
+                  <feGaussianBlur stdDeviation="3" />
+                  <feColorMatrix type="matrix" values="0 0 0 0 0.0901961 0 0 0 0 0.121569 0 0 0 0 0.2 0 0 0 0.1 0" />
+                  <feBlend in2="BackgroundImageFix" mode="normal" result="effect1_dropShadow_230_1510" />
+                  <feBlend in="SourceGraphic" in2="effect1_dropShadow_230_1510" mode="normal" result="shape" />
+                </filter>
+              </defs>
+            </svg>
+          </div>
+          <div className="absolute inset-[10.87%_0_0_1.72%]" data-name="Group">
+            <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 35.3796 32.0868">
+              <g id="Group">
+                <path d={svgPaths.p1f035e00} fill="var(--fill-0, #F83D49)" id="Vector" />
+                <path d={svgPaths.p1565f900} fill="var(--fill-0, #F83D49)" id="Vector_2" />
+                <path d={svgPaths.p3a78b720} fill="var(--fill-0, #F83D49)" id="Vector_3" />
+                <path d={svgPaths.p299a9600} fill="var(--fill-0, #F83D49)" id="Vector_4" />
+              </g>
+            </svg>
+          </div>
+          <div className="absolute bottom-1/2 left-0 right-1/2 top-0" data-name="Vector">
+            <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 17.9999 18">
+              <path d={svgPaths.p39ee4f80} fill="var(--fill-0, #183060)" id="Vector" />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="content-stretch flex flex-col items-start justify-center relative shrink-0 flex-1">
+        <div className="content-stretch flex flex-col gap-[6px] items-start relative w-full">
+          <div className="content-stretch flex flex-col gap-[4px] items-start relative shrink-0 w-full">
+            <div className="content-stretch flex flex-col gap-[2px] items-center justify-center relative shrink-0 w-full" data-name="title-content">
+              <div className="content-stretch flex gap-[8px] items-center relative shrink-0 w-full" data-name="leading-icon+title">
+                <p className="flex-[1_0_0] font-['SF_Pro:Semi_Bold',sans-serif] leading-[16px] min-h-px min-w-px not-italic relative text-[#183060] text-[14px]">New York Lotto</p>
+              </div>
+            </div>
+            <div className="content-stretch flex items-center relative shrink-0 w-full" data-name="Container">
+              <p className="font-['SF_Pro:Regular',sans-serif] font-normal leading-[12px] relative shrink-0 text-[#546d91] text-[10px] whitespace-nowrap" style={{ fontVariationSettings: "'wdth' 100" }}>{`Next draw: Today 14:10 `}</p>
+            </div>
+          </div>
+          <div className="content-stretch flex gap-[6px] h-[16px] items-center relative shrink-0 w-full" data-name="Container">
+            <div className="relative shrink-0 size-[12px]" data-name="Icon">
+              <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 12 12">
+                <g clipPath="url(#clip0_202_1069)" id="Icon">
+                  <path d={svgPaths.p3e7757b0} id="Vector" stroke="var(--stroke-0, #F54900)" strokeLinecap="round" strokeLinejoin="round" />
+                  <path d="M6 3V6L8 7" id="Vector_2" stroke="var(--stroke-0, #F54900)" strokeLinecap="round" strokeLinejoin="round" />
+                </g>
+                <defs>
+                  <clipPath id="clip0_202_1069">
+                    <rect fill="white" height="12" width="12" />
+                  </clipPath>
+                </defs>
+              </svg>
+            </div>
+            <p className="font-['SF_Pro:Regular',sans-serif] font-normal leading-[12px] relative shrink-0 text-[#f54900] text-[10px] whitespace-nowrap" style={{ fontVariationSettings: "'wdth' 100" }}>
+              Starts in 38m 15s
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="content-stretch flex items-center justify-center relative shrink-0">
+        <div className="relative shrink-0 size-[14px]" data-name="arrow-right">
+          <div className="absolute inset-[0_20.85%]" data-name="Vector">
+            <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 8.1625 14">
+              <path d={svgPaths.p2b8f34c0} fill="var(--fill-0, #183060)" id="Vector" />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
