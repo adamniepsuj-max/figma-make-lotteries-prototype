4f80f5ef8e5f8361fdf0066efec237ee8d5fbbce
adamniepsuj-max <adam.niepsuj@888williamhill.com>
2026-06-16 14:55:59 +0100

    Deploy via CodexShare: src/app/components/LotteryFlags.tsx

---
+++ src/app/components/LotteryFlags.tsx
@@ -0,0 +1,218 @@
+import fortyNinesLogo from '../assets/49s-lotto-logo.png';
+import fortyNinesHeaderLogo from '../assets/49s-header-logo.png';
+import dailyMillionsLogo from '../assets/daily-millions-logo.png';
+import flagSvgPaths from '../../imports/LotteriesList/svg-6rmlk7j2s2';
+import usFlagSvgPaths from '../../imports/Lotto-1/svg-7u79nypfi4';
+
+export function IrelandFlag() {
+  return (
+    <div className="w-9 h-9 rounded-full shadow-[0px_0px_6px_0px_rgba(23,31,51,0.1)] relative overflow-hidden bg-white shrink-0">
+      <div className="absolute inset-0 flex">
+        <div className="w-1/3 bg-[#4EB280]" />
+        <div className="w-1/3 bg-white" />
+        <div className="w-1/3 bg-[#FFC35C]" />
+      </div>
+      <div className="absolute border-[#ededed] border-[0.5px] border-solid inset-0 pointer-events-none rounded-full" />
+    </div>
+  );
+}
+
+export function AustraliaFlag() {
+  return (
+    <div className="relative size-9 shrink-0 overflow-hidden rounded-[50px] border-[0.5px] border-[#ededed] shadow-[0_0_6px_rgba(23,31,51,0.1)]">
+      <svg className="absolute inset-0 block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 36 36">
+        <path d={flagSvgPaths.p38ea8480} fill="#183060" />
+      </svg>
+      <div className="absolute bottom-full left-[49.99%] right-1/2 top-0">
+        <svg className="absolute inset-0 block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 0.00223414 0.00003125">
+          <path d={flagSvgPaths.p91f40c0} fill="white" />
+        </svg>
+      </div>
+      <div className="absolute bottom-1/2 left-[49.87%] right-1/2 top-[49.87%]">
+        <svg className="absolute inset-0 block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 0.048163 0.048163">
+          <path d={flagSvgPaths.p2da08a00} fill="white" />
+        </svg>
+      </div>
+      <div className="absolute bottom-1/2 left-0 right-1/2 top-0">
+        <svg className="absolute inset-0 block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 18.0001 17.9999">
+          <path d={flagSvgPaths.p2cfceb00} fill="white" />
+        </svg>
+      </div>
+      <div className="absolute bottom-1/2 left-[6.52%] right-1/2 top-[6.52%]">
+        <svg className="absolute inset-0 block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 15.6522 15.6522">
+          <path d={flagSvgPaths.p3aa28380} fill="#F83D49" />
+        </svg>
+      </div>
+      <div className="absolute bottom-1/2 left-[26.09%] right-1/2 top-[26.09%]">
+        <svg className="absolute inset-0 block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 8.60864 8.6086">
+          <path d={flagSvgPaths.p1b240e00} fill="#F83D49" />
+        </svg>
+      </div>
+      <div className="absolute inset-[58.7%_58.7%_19.57%_19.01%]">
+        <svg className="absolute inset-0 block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 8.0273 7.82606">
+          <path d={flagSvgPaths.p16c4cf40} fill="white" />
+        </svg>
+      </div>
+      <div className="absolute inset-[69.57%_19.57%_19.57%_69.29%]">
+        <svg className="absolute inset-0 block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 4.01365 3.91303">
+          <path d={flagSvgPaths.pa34b00} fill="white" />
+        </svg>
+      </div>
+      <div className="absolute bottom-1/2 left-[56.52%] right-[32.33%] top-[39.13%]">
+        <svg className="absolute inset-0 block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 4.01364 3.91303">
+          <path d={flagSvgPaths.p7df1c0} fill="white" />
+        </svg>
+      </div>
+      <div className="absolute inset-[21.74%_19.57%_67.39%_69.29%]">
+        <svg className="absolute inset-0 block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 4.01358 3.91311">
+          <path d={flagSvgPaths.p32b10300} fill="white" />
+        </svg>
+      </div>
+      <div className="absolute inset-[34.78%_8.42%_54.35%_80.43%]">
+        <svg className="absolute inset-0 block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 4.01365 3.91303">
+          <path d={flagSvgPaths.p2a62e00} fill="white" />
+        </svg>
+      </div>
+      <div className="absolute bottom-[41.3%] left-[73.47%] right-[17.39%] top-1/2">
+        <svg className="absolute inset-0 block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 3.29161 3.13045">
+          <path d={flagSvgPaths.p2b0d4280} fill="white" />
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+export function GermanyFlag() {
+  return (
+    <div className="w-9 h-9 rounded-full shadow-[0px_0px_6px_0px_rgba(23,31,51,0.1)] relative overflow-hidden shrink-0">
+      <div className="absolute inset-0 flex flex-col">
+        <div className="h-1/3 bg-black" />
+        <div className="h-1/3 bg-[#F83D49]" />
+        <div className="h-1/3 bg-[#FFC35C]" />
+      </div>
+      <div className="absolute border-[#ededed] border-[0.5px] border-solid inset-0 pointer-events-none rounded-full" />
+    </div>
+  );
+}
+
+export function CanadaFlag() {
+  return (
+    <div className="relative size-9 shrink-0 overflow-hidden rounded-[50px] border-[0.5px] border-[#ededed] shadow-[0_0_6px_rgba(23,31,51,0.1)]">
+      <div className="absolute inset-[-16.67%]">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 48 48">
+          <path d={flagSvgPaths.p3289ce00} fill="white" />
+        </svg>
+      </div>
+      <div className="absolute inset-[4.96%_0]">
+        <svg className="absolute inset-0 block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 36.0002 32.4278">
+          <path d={flagSvgPaths.p3de8e880} fill="#F83D49" />
+          <path d={flagSvgPaths.p25911700} fill="#F83D49" />
+          <path d={flagSvgPaths.p320a3100} fill="#F83D49" />
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+export function PolandFlag() {
+  return (
+    <div className="w-9 h-9 rounded-full shadow-[0px_0px_6px_0px_rgba(23,31,51,0.1)] relative overflow-hidden bg-white shrink-0">
+      <div className="absolute inset-0 flex flex-col">
+        <div className="h-1/2 bg-white" />
+        <div className="h-1/2 bg-[#F83D49]" />
+      </div>
+      <div className="absolute border-[#ededed] border-[0.5px] border-solid inset-0 pointer-events-none rounded-full" />
+    </div>
+  );
+}
+
+export function FranceFlag() {
+  return (
+    <div className="w-9 h-9 rounded-full shadow-[0px_0px_6px_0px_rgba(23,31,51,0.1)] relative overflow-hidden bg-white shrink-0">
+      <div className="absolute inset-0 flex">
+        <div className="w-1/3 bg-[#183060]" />
+        <div className="w-1/3 bg-white" />
+        <div className="w-1/3 bg-[#F83D49]" />
+      </div>
+      <div className="absolute border-[#ededed] border-[0.5px] border-solid inset-0 pointer-events-none rounded-full" />
+    </div>
+  );
+}
+
+export function DailyMillionsIcon() {
+  return (
+    <div className="relative size-9 shrink-0 overflow-hidden rounded-[50px] shadow-[0_0_6px_rgba(23,31,51,0.1)]">
+      <img
+        alt=""
+        className="absolute h-[109.23%] max-w-none"
+        src={dailyMillionsLogo}
+        style={{
+          left: '-5.57%',
+          top: '-4.36%',
+          width: '110.78%',
+        }}
+      />
+    </div>
+  );
+}
+
+export function FortyNinesIcon() {
+  return (
+    <div className="relative size-9 shrink-0 overflow-hidden rounded-[50px]">
+      <img
+        alt=""
+        className="absolute h-[2850%] max-w-none"
+        src={fortyNinesLogo}
+        style={{
+          left: '-61.7%',
+          top: '-1713.04%',
+          width: '1282.98%',
+        }}
+      />
+    </div>
+  );
+}
+
+export function FortyNinesHeaderIcon() {
+  return (
+    <div className="flex shrink-0 items-center" data-node-id="1312:14597">
+      <div className="relative size-9 shrink-0 overflow-hidden rounded-[50px]" data-node-id="1312:14598">
+        <img
+          alt=""
+          className="absolute h-[2850%] max-w-none"
+          src={fortyNinesHeaderLogo}
+          style={{
+            left: '-61.7%',
+            top: '-1713.04%',
+            width: '1282.98%',
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export function NewYorkIcon() {
+  return (
+    <div className="relative size-9 shrink-0 rounded-[50px] border-[0.5px] border-[#ededed] shadow-[0_0_6px_rgba(23,31,51,0.1)]">
+      <div className="absolute inset-[-16.67%]">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 47.9999 48">
+          <path d={usFlagSvgPaths.p3ef20a80} fill="white" />
+        </svg>
+      </div>
+      <div className="absolute inset-[10.87%_0_0_1.72%] overflow-hidden">
+        <svg className="absolute inset-0 block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 35.3796 32.0868">
+          <path d={usFlagSvgPaths.p1565f900} fill="#F83D49" />
+          <path d={usFlagSvgPaths.p1f035e00} fill="#F83D49" />
+          <path d={usFlagSvgPaths.p299a9600} fill="#F83D49" />
+          <path d={usFlagSvgPaths.p3a78b720} fill="#F83D49" />
+        </svg>
+      </div>
+      <div className="absolute bottom-1/2 left-0 right-1/2 top-0 overflow-hidden">
+        <svg className="absolute inset-0 block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 17.9999 18">
+          <path d={usFlagSvgPaths.p39ee4f80} fill="#183060" />
+        </svg>
+      </div>
+    </div>
+  );
+}
